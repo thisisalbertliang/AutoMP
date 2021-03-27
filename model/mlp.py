e687ca8671e9849fb2c95ca5365fb432732cc48f
Albert Liang <ajliang@andrew.cmu.edu>
2021-03-27 19:23:37 -0400

    fixed typo

--- model/mlp.py
+++ model/mlp.py
@@ -10,7 +10,7 @@ class ParallelMLP(torch.nn.Module):
         self.fc1_relu = ParallelLinear(input_size=num_features, output_size=2048, gather=True)
         self.fc2_relu = ParallelLinear(input_size=2048, output_size=1024, gather=True)
         self.fc3_relu = ParallelLinear(input_size=1024, output_size=512, gather=True)
-        self.fc_final = ParallelLinear(input_size=512, output_size=num_classes, gather=False)
+        self.fc_final = ParallelLinear(input_size=512, output_size=num_classes, gather=True)
 
     def forward(self, X):
         X = self.fc1_relu.forward(X)
